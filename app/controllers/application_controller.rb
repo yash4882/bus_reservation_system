83405535d060e00f2ab572c1c932ee17e3e4d401
Yash Vasundhariya <yashvasundhariya.1@gmail.com>
2023-01-07 13:27:13 +0530

    Extending Devise - Login With Username or Email

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
   before_action :authenticate_user!
   add_flash_types :danger, :info, :warning, :success, :messages
   before_action :configure_permitted_parameters, if: :devise_controller?
@@ -6,7 +8,9 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :contact, :role_id, :age, :gender])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :name, :email, :password, :contact, :role_id, :age, :gender])
+    devise_parameter_sanitizer.permit(:sign_in,keys: [:login, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:account_update,keys: [:username, :name, :email, :password_confirmation, :current_password])
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ApplicationRecord
+  attr_accessor :login
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   rolify
   validates :name, presence: true
+  # validates :username, presence: true, uniqueness: true
+  # validates :username, presence: true, uniqueness: { case_sensitive: false }
   # validates :password, presence: true
   # validates :contact, numericality: {message: "Please Enter numerical value"},length: { minimum: 10, maximum:12 }
   # validates :age, numericality: { message: "%{value} seems wrong" }
@@ -17,8 +20,15 @@ class User < ApplicationRecord
   STATUSES = ['Pending', 'Approved', 'Rejected']
   after_initialize :init
 
+
   def init
     self.status ||= STATUSES[0]
+  end
+
+  def self.find_for_database_authentication warden_condition
+    conditions = warden_condition.dup
+    login = conditions.delete(:login)
+    where(conditions).where(["lower(username) = :value OR lower(email) = :value",{ value: login.strip.downcase}]).first
   end
 
   private
